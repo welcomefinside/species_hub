2df1ec4504538973de195c3e6de8b636e8db6c11
Haixin Shi <hshi83@student.monash.edu>
2019-09-21 18:00:33 +1000

    create train_estimator admin action

--- observation/admin.py
+++ observation/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from observation.models import Observation, Species
 
 # Register your models here.
+def train_estimator(modeladmin, request, queryset):
+    ## passing the quertsets to other views which is doing the training
+    # for item in queryset:
+    #     print(item.ufi)
+    return
+    
 
-admin.site.register(Observation)
+class ObservationAdmin(admin.ModelAdmin):
+    list_display = ['ufi', 'species', 'date_added', 'date_modified']
+    actions = [train_estimator]
+
+admin.site.register(Observation, ObservationAdmin)
 admin.site.register(Species)
