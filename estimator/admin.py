a83a4d387719a1abb6f973dd1883ad048c803d57
Ariffin Baharum <ariffin@realtimeagent.com.au>
2019-10-08 05:30:00 +1100

    train_estimator, removed ipdb, add log messages

--- estimator/admin.py
+++ estimator/admin.py
@@ -24,8 +24,10 @@ from io import BytesIO
 
 
 def train_estimator(modeladmin, request, queryset):
+    print("Training estimator...")
     estimator_obj = queryset[0]
     ## 1. data
+    print("Reading data from selected observations...")
     id_list = estimator_obj.dataset
     data = read_frame(
         Observation.objects.filter(id__in=id_list),
@@ -50,6 +52,7 @@ def train_estimator(modeladmin, request, queryset):
     data = data.replace(to_replace="nan", value="")
 
     ## 2. tag dictionary
+    print("Preparing tag dictionary...")
     binary_columns = ["sampling_method_desc", "record_type", "lga_ufi", "cma_no"]
     integer_columns = ["lat_long_accuracydd_int", "sv_record_count"]
     target_column = ["reliability"]
@@ -61,6 +64,7 @@ def train_estimator(modeladmin, request, queryset):
     }
 
     ## 3. configuration_dict
+    print("Preparing configuration dictionary...")
     # search_parameters
     search_parameters = dict(
         scoring="roc_auc",
@@ -94,6 +98,7 @@ def train_estimator(modeladmin, request, queryset):
     }
 
     ## 4. cc_pipe_kwargs
+    print("Preparing cleanse and consolidation pipe arguments...")
     cond_imp_args = dict(tag_dict=tag_dict, conditioning_column=tag_dict["id"][0])
 
     binariser_args = dict(binary_columns=tag_dict["binary"], id_col="species")
@@ -101,39 +106,37 @@ def train_estimator(modeladmin, request, queryset):
     cc_pipe_kwargs = dict(fit_arg_list=[cond_imp_args, binariser_args])
 
     ## 5. split_type
-
+    print("Preparing split dictionary...")
     split_type = estimator_obj.split_type
 
     # 6. split_kwargs
 
-    if split_type == "tvt":
+    if split_type == "none":
         split_kwargs = {
-            "test_size": estimator_obj.test_size,
-            "validate_size": estimator_obj.validate_size,
-            "stratify": ["species", "reliability"],
-        }
-    elif split_type == "tt":
-        split_kwargs = {
-            "test_size": estimator_obj.test_size,
+            "test_size": 0.33,
             "stratify": ["species", "reliability"],
         }
     else:
-        split_kwargs = {"test_size": 0.33, "stratify": ["species", "reliability"]}
-
+        split_kwargs = {
+            "test_size": estimator_obj.test_size,
+            "stratify": ["species", "reliability"],
+        }
+    
+    print("Initializing estimator...")
     delwp_estimator = DWELPModel(
         data, tag_dict, configuration_dict, cc_pipe_kwargs, split_type, split_kwargs
     )
 
+    print("Pickling estimator...")
     estimator_file_name = "estimator.pkl"
     estimator_pkl = pickle.dumps(delwp_estimator)
 
-    import ipdb
-
-    ipdb.set_trace()
-
+    print("Creating TrainedEstimator object...")
     trained_estimator_object, created = TrainedEstimator.objects.update_or_create(
         id=uuid.uuid4(), pickled_estimator=estimator_pkl
     )
+
+    print("Saving TrainedEstimator object...")
     trained_estimator_object.save()
     return
 
